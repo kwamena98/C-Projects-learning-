27bffd887c672b15d00a4ec3e5869a8849099acc
kwamen98 <derrickdadson14@gmail.com>
2024-05-28 16:58:11 +0000

    Errors and updated Firstprogram with comments

---
+++ Errors.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    /*
+
+    Compile Error :
+    During compilation of your code thus converting it into binary when there is a compile error in your code we get error and therefore the executable file is not produced
+    because the executable files are only produced when compilation is successful An example of such error is below :
+    **/
+
+    cout<<"Derrick Dadson"<<endl
+
+    /*
+
+    Runtime Error :
+
+    Runtime Error is a type of error that builds successfully but after building or compilation we get such errors 
+    An example of such error is below
+
+    **/
+
+   int test=7/0;
+   cout<<"The result is :" <<test<<endl;
+
+
+
+
+
+
+
+}
